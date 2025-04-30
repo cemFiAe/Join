e36c9a435f5a549d42a32813bd2bd3f114be2cda
CaticJasmin <caticjasmin92@gmail.com>
2025-04-30 13:10:30 +0200

    join logo move effect added

--- scripts/log_in_loading.js
+++ scripts/log_in_loading.js
@@ -1,7 +1,5 @@
 
 function showLoadingScreen() {
-    document.getElementById('overlay').classList.add('loading-overlay');
-    document.getElementById('log-in-logo').style.display = "block";
     setTimeout(function () {
         document.getElementById('overlay').style.display = "none";
         document.getElementById('main-content').style.display = "block";    
