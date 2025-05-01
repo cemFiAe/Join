13ea7b7aec0949c6733296bc88816da079b59eb9
CaticJasmin <caticjasmin92@gmail.com>
2025-05-01 18:08:41 +0200

    log in loading effect in mobile and full page added, needs adjustment

--- scripts/log_in_loading.js
+++ scripts/log_in_loading.js
@@ -2,6 +2,20 @@
 function showLoadingScreen() {
     setTimeout(function () {
         document.getElementById('overlay').style.display = "none";
-        document.getElementById('main-content').style.display = "block";    
-    }, 500);
+        document.getElementById('main-content').style.display = "block"; 
+    }, 750);
 }
+
+
+// function showLoadingScreen() {
+//     let overlay = document.getElementById('overlay');
+//     let logo = document.querySelector('.mobile-log-in-logo');
+//     let mainContent = document.getElementById('main-content');
+//     let mobileHeaderLogo = document.getElementById('mobile-header-logo');
+
+//     // Show header logo and main content after delay
+//     setTimeout(() => {
+//         mobileHeaderLogo.style.display = "block";
+//         mainContent.style.display = "block";
+//     }, 500);
+// }
