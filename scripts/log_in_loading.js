2f8c1d22d44f7706ebdd0b2bb288ffa7f8380fe9
CaticJasmin <caticjasmin92@gmail.com>
2025-05-04 13:01:27 +0200

    log in responsive 320px finished and  mobile updated

--- scripts/log_in_loading.js
+++ scripts/log_in_loading.js
@@ -6,16 +6,3 @@ function showLoadingScreen() {
     }, 500);
 }
 
-
-// function showLoadingScreen() {
-//     let overlay = document.getElementById('overlay');
-//     let logo = document.querySelector('.mobile-log-in-logo');
-//     let mainContent = document.getElementById('main-content');
-//     let mobileHeaderLogo = document.getElementById('mobile-header-logo');
-
-//     // Show header logo and main content after delay
-//     setTimeout(() => {
-//         mobileHeaderLogo.style.display = "block";
-//         mainContent.style.display = "block";
-//     }, 500);
-// }
