c515e4839f6c3304abcc1134711db22305bcf7c9
CaticJasmin <caticjasmin92@gmail.com>
2025-05-07 19:04:25 +0200

    sign up overlay not finished

--- scripts/signed_up.js
+++ scripts/signed_up.js
@@ -1,5 +1,13 @@
+// function signedUp() {
+//     document.getElementById('signed-up').classList.add('signed-up-screen')
+//     setTimeout(() => {
+//         document.getElementById('signed-up').classList.remove('signed-up-screen')
+//     }, 1000);
+// }
+
 function signedUp() {
+    document.getElementById('signed-up').style.display = "block"
     setTimeout(() => {
         document.getElementById('signed-up').style.display = "none"
-    }, 500);
+    }, 5000);
 }
