2796e64879d49b98d5fe20cbfa9959b11de7ccc6
itzveo <veox1use@web.de>
2025-05-09 13:12:18 +0200

    mobile_menu.js working, small adjusts

--- scripts/mobile_menu.js
+++ scripts/mobile_menu.js
@@ -1,10 +1,29 @@
-let menuToggle = document.getElementById('menuToggle');
-let mobileNav = document.getElementById('mobileNav');
+const menuToggle = document.getElementById("menuToggle");
+const mobileNav = document.getElementById("mobileNav");
+let menuOpen = false;
 
-function toggleMenu() {
-
+function openMenu() {
+  mobileNav.style.left = "74%";
+  menuOpen = true;
+  document.addEventListener("click", handleClickOutside);
 }
 
 function closeMenu() {
-    
+  mobileNav.style.left = "100%";
+  menuOpen = false;
+  document.removeEventListener("click", handleClickOutside);
 }
+
+function toggleMenu(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  menuOpen ? closeMenu() : openMenu();
+}
+
+function handleClickOutside(event) {
+  if (!mobileNav.contains(event.target) && event.target !== menuToggle) {
+    closeMenu();
+  }
+}
+
+menuToggle.addEventListener("click", toggleMenu);
