50ad070723ca77ebb23806595f5bd5980c786cf2
CaticJasmin <caticjasmin92@gmail.com>
2025-05-09 20:34:46 +0200

    privacy policy and legal notice (guest&normal) adjusted, sign up button function needs adjustment

--- scripts/signed_up.js
+++ scripts/signed_up.js
@@ -1,8 +1,14 @@
 
 function signedUp() {
+    let form = document.getElementById('sign-up-form')
+    if (!form.checkValidity()) {
+        return false;
+    }
+
     document.getElementById('signed-up').style.display = "block"
     setTimeout(() => {
         document.getElementById('signed-up').style.display = "none"
         window.location.href = "../pages/log_in.html";
     }, 1000);
+    return true;
 }
