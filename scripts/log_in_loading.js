f9f9ce1024e9336085bdf67e2af3827854563c3b
CaticJasmin <caticjasmin92@gmail.com>
2025-05-10 18:20:15 +0200

    sign up button function fixed

--- scripts/log_in_loading.js
+++ scripts/log_in_loading.js
@@ -1,8 +1,16 @@
-
 function showLoadingScreen() {
     setTimeout(function () {
         document.getElementById('overlay').style.display = "none";
-        document.getElementById('main-window').style.display = "block"; 
+        document.getElementById('main-window').style.display = "block";
     }, 500);
 }
 
+function checkLoadingScreen() {
+    if (!sessionStorage.getItem('cameFromSignUp')) {
+        showLoadingScreen();
+    } else {
+        document.getElementById('overlay').style.display = "none";
+        document.getElementById('main-window').style.display = "block";
+        sessionStorage.removeItem('cameFromSignUp');
+    }
+}
--- scripts/signed_up.js
+++ scripts/signed_up.js
@@ -1,14 +1,22 @@
+function signedUp(event) {
+    event.preventDefault();
 
-function signedUp() {
-    let form = document.getElementById('sign-up-form')
+    let form = document.getElementById('sign-up-form');
     if (!form.checkValidity()) {
         return false;
     }
+    
+    let checkbox = document.getElementById('accept-privacy-policy');
+    if (!checkbox.checked) {
+        alert('You must accept the privacy policy!');
+        return false;
+    }
 
-    document.getElementById('signed-up').style.display = "block"
+    document.getElementById('signed-up').style.display = "block";
     setTimeout(() => {
-        document.getElementById('signed-up').style.display = "none"
+        document.getElementById('signed-up').style.display = "none";
         window.location.href = "../pages/log_in.html";
+        sessionStorage.setItem('cameFromSignUp', 'true');
     }, 1000);
     return true;
 }
