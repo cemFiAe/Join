c5833706550cb23a33c36a97e61f844127ab09e5
CaticJasmin <caticjasmin92@gmail.com>
2025-05-13 20:18:37 +0200

    js coding for log in and sign up page, not finished

--- scripts/show_password.js
+++ scripts/show_password.js
@@ -1,5 +1,26 @@
+function insertPassword() {
+    let pass = document.getElementById('password')
+    document.querySelector('.password').classList.add('blue-frame')
+    if (pass.type === "password") {
+        pass.type = "password";
+        document.getElementById('lock-logo').src = "../assets/icons/log_in/visibility_off.svg"
+    } else if (pass.value === "")
+        document.getElementById('lock-logo').src = "../assets/icons/log_in/lock.svg"
+}
+
 function showPassword() {
     let pass = document.getElementById('password')
+    if (pass.type === "password") {
+        pass.type = "text";
+        document.getElementById('lock-logo').src = "../assets/icons/log_in/visibility.svg"
+    } else {
+        pass.type = "password";
+        document.getElementById('lock-logo').src = "../assets/icons/log_in/visibility_off.svg"
+    }
+}
+
+function confirmPassword() {
+    let pass = document.getElementById('confirm-password')
     if (pass.type === "password") {
         pass.type = "text";
     } else {
