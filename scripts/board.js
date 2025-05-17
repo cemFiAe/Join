58e34caaa14664cf1204cb4dcf7c9d7ff9808b4a
itzveo <veox1use@web.de>
2025-05-17 20:13:09 +0200

    started add task overlay

---
+++ scripts/board.js
@@ -0,0 +1,7 @@
+function openAddTaskOverlay() {
+    document.getElementById('add_task_overlay').style.display = 'flex';
+}
+
+function closeAddTaskOverlay() {
+    document.getElementById('add_task_overlay').style.display = 'none';
+}
