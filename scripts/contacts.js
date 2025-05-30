24ca53a206d5bf600cb093161eee0cc1ab77c894
itzveo <veox1use@web.de>
2025-05-30 14:09:14 +0200

    added edit contact overlay, js needs fix

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,5 +1,8 @@
+// add contact overlay
+
 function openAddContact() {
     document.getElementById('add_contact_overlay').style.left = "5%";
+    document.getElementById('add_contact_overlay').style.top = "20%";
     document.getElementById('bg_overlay').style.display = "flex";
 }
 
@@ -21,10 +24,31 @@ async function closeAddContactMobile() {
     document.getElementById('add_contact_btn').style.display = "flex";
 }
 
+document.getElementById('add_contact_overlay').addEventListener('click', function (event) {
+    event.stopPropagation();
+});
+
 document.getElementById('bg_overlay').addEventListener('click', function () {
     closeAddContact();
 });
 
-document.getElementById('add_contact_overlay').addEventListener('click', function (event) {
+// edit contact overlay
+
+function openEditContact() {
+    document.getElementById('edit_contact_overlay').style.left = "5%";
+    document.getElementById('edit_contact_overlay').style.top = "20%";
+    document.getElementById('bg_overlay').style.display = "flex";
+}
+
+function closeEditContact() {
+    document.getElementById('edit_contact_overlay').style.left = "105%";
+    document.getElementById('bg_overlay').style.display = "none";
+}
+
+document.getElementById('bg_overlay').addEventListener('click', function () {
+    closeEditContact();
+});
+
+document.getElementById('edit_contact_overlay').addEventListener('click', function (event) {
     event.stopPropagation();
 });
