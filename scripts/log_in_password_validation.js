50d8cb207c3eda3c48f8488596d429f0778e011c
CaticJasmin <caticjasmin92@gmail.com>
2025-06-03 20:50:54 +0200

    error at log in buttons cleared

--- scripts/log_in_password_validation.js
+++ scripts/log_in_password_validation.js
@@ -7,6 +7,37 @@ function isPasswordValid(value) {
     return value.length >= 8 && letters >= 6 && numbers >= 2;
 }
 
+// Check input, and perform accordingly:
+document.addEventListener('input', function (e) {
+    let passwordDiv = document.querySelector('.log-in-password');
+    let passwordInput = document.getElementById('log-in-password');
+    let iconElement = document.getElementById('log-in-lock-icon');
+    let passwordAlert = document.getElementById('log-in-password-alert')
+
+    // Check if input is true --->
+    if (passwordDiv && passwordDiv.contains(e.target)) {
+        passwordDiv.style.borderColor = 'rgb(255, 0, 31)';
+        iconElement.src = "../assets/icons/log_in/visibility_off.svg";
+
+        // ---> check the input/password and highlight accordingly;
+        if (isPasswordValid(passwordInput.value)) {  // if valid:
+            passwordDiv.style.borderColor = 'rgb(41, 171, 226)';
+            passwordAlert.innerHTML = "";
+
+        } else {  // if not valid:
+            passwordDiv.style.borderColor = 'rgb(255, 0, 31)';
+            passwordAlert.innerHTML = "Please insert correct password";
+        }
+
+        // if input is empty:
+        if (passwordInput.value === "") {
+            passwordDiv.style.borderColor = 'rgba(0, 0, 0, 0.1)';
+            passwordAlert.innerHTML = "";
+            iconElement.src = "../assets/icons/log_in/lock.svg";
+        }
+    }
+});
+
 // Log in password visibility and visibility matching icon:
 function logInVisibility(iconElement) {
 
@@ -49,33 +80,3 @@ document.addEventListener('click', function (e) {
 
 });
 
-// Check input, and perform accordingly:
-document.addEventListener('input', function (e) {
-    let passwordDiv = document.querySelector('.log-in-password');
-    let passwordInput = document.getElementById('log-in-password');
-    let iconElement = document.getElementById('log-in-lock-icon');
-    let passwordAlert = document.getElementById('log-in-password-alert')
-
-    // Check if input is true --->
-    if (passwordDiv && passwordDiv.contains(e.target)) {
-        passwordDiv.style.borderColor = 'rgb(255, 0, 31)';
-        iconElement.src = "../assets/icons/log_in/visibility_off.svg";
-
-        // ---> check the input/password and highlight accordingly;
-        if (isPasswordValid(passwordInput.value)) {  // if valid:
-            passwordDiv.style.borderColor = 'rgb(41, 171, 226)';
-            passwordAlert.innerHTML = "";
-
-        } else {  // if not valid:
-            passwordDiv.style.borderColor = 'rgb(255, 0, 31)';
-            passwordAlert.innerHTML = "Please insert correct password";
-        }
-
-        // if input is empty:
-        if (passwordInput.value === "") {
-            passwordDiv.style.borderColor = 'rgba(0, 0, 0, 0.1)';
-            passwordAlert.innerHTML = "";
-            iconElement.src = "../assets/icons/log_in/lock.svg";
-        }
-    }
-});
