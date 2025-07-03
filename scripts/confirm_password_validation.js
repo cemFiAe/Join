a864eb79a492e15378444cb41c550031d6c11f96
CaticJasmin <caticjasmin92@gmail.com>
2025-07-03 20:33:00 +0200

    started with JSDoc

--- scripts/confirm_password_validation.js
+++ scripts/confirm_password_validation.js
@@ -1,5 +1,6 @@
-
-// Confirm password validation for Sign up page
+/**
+ * test
+ */
 
 var confirmDiv = document.querySelector('.confirm-password');
 var confirmInput = document.getElementById('confirm-password');
@@ -7,10 +8,19 @@ var confirmIcon = document.getElementById('confirm-lock-icon');
 var confirmAlert = document.getElementById('confirm-password-alert');
 var passwordInput = document.getElementById('sign-up-password');
 
-// Check confirm input, validate it and update styles accordingly:
+
+/**
+ * This event listener checks the confirm password input, then validates if it matches the password input
+ * and based on validation updates the styles of confirm password field.
+ * 
+ * @param password - User enters his password.
+ * @param confirmPassword - User confirms his password.
+ * @param isMatch - This is the main condition user must fulfill to sign in.
+ */
+
 confirmInput.addEventListener('input', function () {
-    let confirmPassword = confirmInput.value;
     let password = passwordInput.value;
+    let confirmPassword = confirmInput.value;
 
     if (confirmPassword === "") {
         confirmDiv.style.borderColor = 'rgb(255, 0, 31)';
@@ -25,11 +35,20 @@ confirmInput.addEventListener('input', function () {
 });
 
 
-// Check if the icon in input filed is clicked and: 
-// toggle password visibility and matching icon,
+/**
+ * Event listener checks if user clicks on the lock-icon which is located in the password field,
+ * and if yes, it calls confirmPasswordVisibility() function.
+ * Otherwise, if somewhere else in page is clicked, it ensures that the password is no longer visible,
+ * and shows back lock-icon.
+ * 
+ * @param e.target.id - Represents the id of element on which is clicked.
+ * @param confirmInput.type - If type is "password", then the password is not visible, if type is "text" then it's visible.
+ * @param confirmIcon.src - URL of displayed icon.
+ */
+
 document.addEventListener('click', function (e) {
     if (e.target.id === 'confirm-lock-icon') {
-        confirmVisibility(confirmInput, e.target);
+        confirmPasswordVisibility(confirmInput, e.target);
     } else if (!confirmDiv.contains(e.target)) {
         confirmInput.type = "password";
         confirmIcon.src = "../assets/icons/sign_up/confirm_lock.svg";
@@ -37,8 +56,16 @@ document.addEventListener('click', function (e) {
 });
 
 
-// Function for changing password visibility in confirm field and matching icon
-function confirmVisibility(input, icon) {
+/**
+ * This function determines that password and icon are either visible or not visible 
+ * and regarding to visibility, updates it's two parameters.
+ * The click event above toggles that visibility.
+ * 
+ * @param {string} input.type - Depending on input type, input can be visible or not visible.
+ * @param {icon} icon.src - These URLs are matching icons for each input type.
+ */
+
+function confirmPasswordVisibility(input, icon) {
     if (input.type === "password") {
         input.type = "text";
         icon.src = "../assets/icons/sign_up/confirm_visibility.svg";
