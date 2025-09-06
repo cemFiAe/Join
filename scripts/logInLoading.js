f7102d19c0966d51e008c2e3feadac66827a56c0
CaticJasmin <caticjasmin92@gmail.com>
2025-09-06 13:30:51 +0200

    adjusting log in start animation

--- scripts/logInLoading.js
+++ scripts/logInLoading.js
@@ -1,15 +1,21 @@
 
 let overlay = document.getElementById('overlay')
 let mainWindow = document.getElementById('main-window')
+let headerLogo = document.getElementById('header-logo')
+let noLogo = document.getElementById('no-logo')
 /**
  * This function ensures that overlay with message shows up 0.5s
  * when the user successfully signs up.
  */
 function showLoadingScreen() {
     setTimeout(function () {
-        overlay.style.display = "none";
         mainWindow.style.display = "block";
-    }, 500);
+        headerLogo.style.display = "block";
+    }, 200);
+    noLogo.style.display = "none";
+    setTimeout(function () {
+        overlay.style.display = "none";
+    }, 800)
 }
 
 
@@ -19,7 +25,7 @@ function showLoadingScreen() {
  * This function checks if user didn't came from sign up! (but other two ways)
  * If so, it calls showLoadingScreen function.
  * But if the user came from sign up, there is no overlay.
- */
+*/
 function checkLoadingScreen() {
     if (!sessionStorage.getItem('cameFromSignUp')) {
         showLoadingScreen();
